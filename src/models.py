01ae92069d886814b7177e32204a97f8d01db5ff
Bissapinho <alexandregalle3@gmail.com>
2026-03-11 00:19:35 +0100

    CI, HP search, walk forward

--- src/models.py
+++ src/models.py
@@ -5,13 +5,19 @@ import numpy as np
 
 class RFCModel:
 
-    def __init__(self, n_estimators=100, max_depth=5, 
-                 min_samples_split=2, min_samples_leaf=1):
+    def __init__(self, 
+            n_estimators=346,
+            max_depth=9,
+            min_samples_split=17,
+            min_samples_leaf=5,
+            max_features=0.5463,
+        ):
         self.params = dict(
             n_estimators=n_estimators,
             max_depth=max_depth,
             min_samples_split=min_samples_split,
             min_samples_leaf=min_samples_leaf,
+            max_features=max_features,
             class_weight='balanced',
             random_state=42
         )
@@ -31,15 +37,16 @@ class RFCModel:
     def feature_importances_(self):
         return self.model.feature_importances_
 
-
 class XGBModel:
 
     def __init__(self, 
-            n_estimators=300,
-            max_depth=4,
-            learning_rate=0.05,
-            subsample=0.8,
-            colsample_bytree=0.8,
+            n_estimators=138,
+            max_depth=5,
+            learning_rate=0.0430,
+            subsample=0.7273,
+            colsample_bytree=0.6976,
+            gamma=0.0536,
+            min_child_weight=1,
             eval_metric='logloss',
             verbosity=0
         ):
@@ -48,10 +55,11 @@ class XGBModel:
         self.learning_rate = learning_rate
         self.subsample = subsample
         self.colsample_bytree = colsample_bytree
+        self.gamma = gamma
+        self.min_child_weight = min_child_weight
         self.eval_metric = eval_metric
         self.verbosity = verbosity
         self.model = None
-
 
     def fit(self, X_train, y_train):
         spw = (y_train == 0).sum() / (y_train == 1).sum()
@@ -61,10 +69,12 @@ class XGBModel:
             learning_rate=self.learning_rate,
             subsample=self.subsample,
             colsample_bytree=self.colsample_bytree,
+            gamma=self.gamma,
+            min_child_weight=self.min_child_weight,
             scale_pos_weight=spw,
+            eval_metric=self.eval_metric,
             random_state=42,
-            eval_metric='aucpr',
-            verbosity=0
+            verbosity=self.verbosity
         )
         self.model.fit(X_train, y_train)
         return self
